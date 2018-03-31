a5fa49e7582360c799c128727d63b1127c441653
Vladimir Zapletal <vlad.zapletal@gmail.com>
2018-03-31 08:40:20 +0200

    Added more HTTP methods

--- src/Http/Method.php
+++ src/Http/Method.php
@@ -6,13 +6,21 @@ final class Method
 {
     const GET_METHOD = 'GET';
     const POST_METHOD = 'POST';
+    const PATCH_METHOD = 'PATCH';
+    const DELETE_METHOD = 'DELETE';
+
     /**
      * @var string
      */
     private $method;
 
+    /**
+     * @param string $method
+     */
     public function __construct(string $method)
     {
+        $method = strtoupper($method);
+
         $this->method = $method;
     }
 
@@ -24,16 +32,45 @@ final class Method
         return strtoupper($this->method);
     }
 
-
+    /**
+     * @param Method $method
+     *
+     * @return bool
+     */
     public function sameAs(Method $method): bool
     {
         return $method->getMethod() === $this->getMethod();
     }
 
+    /**
+     * @return Method
+     */
     public static function GET(): self
     {
         return new self(self::GET_METHOD);
     }
 
+    /**
+     * @return Method
+     */
+    public static function POST(): self
+    {
+        return new self(self::POST_METHOD);
+    }
 
+    /**
+     * @return Method
+     */
+    public static function PATCH(): self
+    {
+        return new self(self::PATCH_METHOD);
+    }
+
+    /**
+     * @return Method
+     */
+    public static function DELETE(): self
+    {
+        return new self(self::DELETE_METHOD);
+    }
 }
