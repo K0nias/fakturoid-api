a304aeee738b57b99cc1ac3934106303e00b245f
Vladimir Zapletal <vlad.zapletal@gmail.com>
2018-08-01 14:54:35 +0200

    CreateInvoiceResponse: added ::getId() method to get created invoice ID

--- src/Http/Response/Invoice/CreateInvoiceResponse.php
+++ src/Http/Response/Invoice/CreateInvoiceResponse.php
@@ -3,7 +3,14 @@
 namespace K0nias\FakturoidApi\Http\Response\Invoice;
 
 use K0nias\FakturoidApi\Http\Response\CommonJsonResponse;
+use K0nias\FakturoidApi\Model\Invoice\Id;
 
 final class CreateInvoiceResponse extends CommonJsonResponse
 {
+
+    public function getId(): Id
+    {
+        return new Id($this->data['id']);
+    }
+
 }
