2b54cd416b39515664d712dd11ca1efe552fc38d
Vladimir Zapletal <vlad.zapletal@gmail.com>
2018-08-01 13:22:49 +0200

    CreateSubjectResponse: added ::getSubjectId() method

--- src/Http/Response/Subject/CreateSubjectResponse.php
+++ src/Http/Response/Subject/CreateSubjectResponse.php
@@ -3,6 +3,13 @@
 namespace K0nias\FakturoidApi\Http\Response\Subject;
 
 use K0nias\FakturoidApi\Http\Response\CommonJsonResponse;
+use K0nias\FakturoidApi\Model\Subject\Id;
 
 final class CreateSubjectResponse extends CommonJsonResponse
-{}
+{
+
+    public function getSubjectId(): Id
+    {
+        return new Id($this->data['id']);
+    }
+}
